abd7a1cb94ce9e17d68a3f383db2f22963875008
Eric <eric.harkat@hotmail.fr>
2020-03-19 18:21:29 +0100

    jeu de test

--- codes_sources/brain/tests/fr/iotiaquarium/testsJbrain/TestIntegration.java
+++ codes_sources/brain/tests/fr/iotiaquarium/testsJbrain/TestIntegration.java
@@ -28,23 +28,46 @@ public class TestIntegration {
 			}
 			
 		};
-		
+		//instanciation d'un neurone
 		Neuron<Object> n1 = new Neuron<Object>(task1);
+		//un neuron est toujour lié à un moniteur qui monitor le resulat final
 		NeuronMonitor<Object> monitor = new NeuronMonitor<Object>(n1);
-		
-		
+		//demarrage du neuronne (start car est considéré comme une stread)
 		n1.start();
-		
-		
 		Object result = monitor.getResult();
-		
-		
-		
 		assertEquals(result, 4);
-		
-
 	}
 	
+	
+
+	@Test
+	@DisplayName("Test test jbrian2")
+public final void testSample() { 
+	
+	 ArrayList<Object> l1 = new ArrayList<Object>();
+	    l1.add(12);
+	    l1.add("toto ! !");
+	    l1.add(12.20f);
+		
+		NeuronTask<Object> task2 = new NeuronTask<Object>() {
+
+			@Override
+			public Object execute(ArrayList<Object> inputs) {
+
+				Object result = ((Integer)inputs.get(0)*((float)inputs.get(2)));
+				
+				return result;
+			}
+			
+		};
+		
+		Neuron<Object> n2 = new Neuron<Object>(task2);
+		n2.setInputs(l1);
+		NeuronMonitor<Object> monitor = new NeuronMonitor<Object>(n2);
+		n2.start();
+		Object result = monitor.getResult();
+		assertEquals(result, 146.4f);
+	}
 	
 	
 }
