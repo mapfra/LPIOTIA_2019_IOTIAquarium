33a7e4a3527933dac03d70f1a8943ada8225e43c
Eric <eric.harkat@hotmail.fr>
2020-04-10 14:41:28 +0200

    ajout vue données, + ajout js pour niveau d'eau, + ajout temperature eau
     ajout ph + ajout  js ajout d'aquarium

--- codes_sources/app/js/index.js
+++ codes_sources/app/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('load_aquarium', this.load_aquarium.bind(this), false);
     },
 
     // deviceready Event Handler
@@ -30,6 +31,51 @@ var app = {
 
     },
 
+    load_aquarium: function () {
+
+        console.log("ok start event");
+
+        let aquarium_list = $(".aquarium");
+
+        console.log(aquarium_list);
+
+        if (aquarium_list.length==0){
+            let parent = $("#aquarium_parent");
+            parent.append("<p id='rien_ici'>Il n'y a rien ici !</p>");
+        }else{
+            $("#rien_ici").remove();
+            //  Dynamic loading
+        }
+
+        let me = this;
+        $("#aquarium_add").on('click', function () {
+            me.add_aquarium();
+        })
+    },
+
+    add_aquarium: function ()  {
+
+        // get parrent
+        let parent = $("#aquarium_parent");
+
+        //  on ajoute l'enfant
+        parent.append("<form id='aquarium_id'>" +
+            "<a href=\"aquarium.html?aquarium=aquarium_id\">" +
+            "<section class=\"aquarium bg-blue\" >" +
+            "<h2 class=\"aquarium-name\">Name</h2>" +
+            "<table class=\"aquarium-details\">"+
+            "<tr>"+
+            "<td class=\"aquarium-details-item\">Status</td>"+
+            "<td class=\"aquarium-details-item\">Niveau eau</td>"+
+            "<td class=\"aquarium-details-item\">Temperature</td>"+
+            "<td class=\"aquarium-details-item\">Lumiere</td>"+
+            "</tr>"+
+            "</table>"+
+            "</section>"+
+            "</a>" +
+            "</form>");
+
+    },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 
