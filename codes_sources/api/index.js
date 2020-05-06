30d19fb812e68f92591bd57995fcad10e4843cfb
Eric <eric.harkat@hotmail.fr>
2020-05-06 14:24:45 +0200

    integration influxdb  + test

--- codes_sources/api/index.js
+++ codes_sources/api/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const Influx = require('influxdb-nodejs');
+const bodyParser = require('body-parser');
 const client = new Influx('http://127.0.0.1:8086/metrics');
 const iotiaquarium = require("./iotiaquarium");
 
 
 const app= express();
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // recuperation des aquarium
 
 //récupération des données
@@ -13,14 +16,31 @@ const app= express();
 //création de la version de l'API
 const versionAPI = '/api/v1';
 
+// tableau utilisateur
+//TODO: sauvegarder dans un fichier Json
+const users = [];
+
+users.push({
+    userid:1,
+    aquariums:[
+        {id:1, sensors:[], triggers:[], deleted: false},
+        {id:2, sensors:[], triggers:[], deleted: false}
+    ]
+});
+
 
 
 //recuperation des aquarium
-app.get(`${versionAPI}/user/:id/aquarium`, (req, res) =>{
+app.get(`${versionAPI}/users/:id/aquariums`, (req, res) =>{
     var id = req.params.id;
+    let aquariums = [];
+    let user = iotiaquarium.getUserById(users,id);
+    if(user!=undefined){
+        aquariums = user.aquariums;
+    }
     // get barer
     res.json({
-        data: iotiaquarium.getUserById(client,id).aquariums || null,
+        data:  aquariums || null,
         status:true
     })
 });
@@ -29,21 +49,55 @@ app.get(`${versionAPI}/user/:id/aquarium`, (req, res) =>{
 app.get(`${versionAPI}/aquarium/:id`, (req, res) =>{
     var id = req.params.id;
     res.json({
-        data: iotiaquarium.getAquariumById(id).aquariums || null
+        data: iotiaquarium.getAquariumById(users,id) || null
 
     })
 });
 
-//recuperation de l'aquarium via l'id
-app.delete(`${versionAPI}/aquarium/:id`, (req, res) =>{
-   user.splice(id,1);
-   res.sendStatus(200);
+
+//  Insersion d'une valeur pour le ph_sensor
+app.post(`${versionAPI}/users/:userid/aquariums/:aquariumid/ph_sensor`, (req, res) =>{
+    let userid = req.params.userid;
+    let aquariumid = req.params.aquariumid;
+    let aquarium = iotiaquarium.getAquariumById(users,aquariumid);
+    let value = req.body.value;
+
+    let status = false;
+
+    if(aquarium!=undefined){
+        iotiaquarium.insertPhValuesByAquariumId(client,aquariumid,userid,value);
+        status = true;
+    }
+    // get barer
+    res.json({
+        data:  value,
+        status:status
+    })
 });
+
+//  Recuperation valeurs de ph_sensor
+app.get(`${versionAPI}/users/:userid/aquariums/:aquariumid/ph_sensor`, (req, res) =>{
+    let userid = req.params.userid;
+    let aquariumid = req.params.aquariumid;
+    let aquarium = iotiaquarium.getAquariumById(users,aquariumid);
+
+    if(aquarium!=undefined){
+        iotiaquarium.getPhValuesByAquariumId(client,aquariumid,userid, res);
+    }else{
+        // get barer
+        res.json({
+            data:  null,
+            status:false
+        })
+    }
+
+});
+
 
 app.get(`${versionAPI}/aquarium/:id/datas`, (req, res) =>{
     var id = req.params.id;
     res.json({
-        data: iotiaquarium.getUserByIdDatas(id).aquariums || null
+        data: iotiaquarium.getDataByAquariumId(id).aquariums || null
     })
 });
 
@@ -62,5 +116,63 @@ app.post(`${versionAPI}/aquarium/:id/food`, (req, res) =>{
 });
 
 
+//  On cree un utilisateur
+app.post(`${versionAPI}/users`, (req, res) =>{
 
+    let userId = req.body.userId;
+
+    users.push({
+        userId:userId,
+        aquariums: []
+    });
+
+    res.json({
+        data: iotiaquarium.getUserById(id) || null
+    })
+});
+
+//  On ajoute un aquarium à un utilisateur
+app.post(`${versionAPI}/users/:id/aquariums`, (req, res) =>{
+
+    let userId = req.body.userId;
+    let aquariumId = req.body.aquariumId;
+
+    let aquarium = {
+        id: aquariumId,
+        sensors:[],
+        triggers:[]
+    };
+    let user = iotiaquarium.getUserById(userId);
+
+    let status = false;
+
+    if (user!=undefined){
+        user.aquariums.push(aquarium);
+        status=true;
+    }
+
+    res.json({
+        data: aquarium,
+        status:status
+    });
+});
+
+//  On supprime un aquarium
+//delete de l'aquarium via l'id
+app.delete(`${versionAPI}/aquarium/:id`, (req, res) =>{
+    let aquariumId = req.params.id;
+    let aquarium = iotiaquarium.getAquariumById(users,aquariumId);
+
+    let status = false;
+
+    if (aquarium!==undefined){
+        aquarium.deleted = true;
+        status=true;
+    }
+
+    res.json({
+        data: aquarium,
+        status:status
+    });
+});
 app.listen(3000, () =>console.log('listen on port 3000'));
--- codes_sources/api/iotiaquarium.js
+++ codes_sources/api/iotiaquarium.js
@@ -1,3 +1,5 @@
+
+
 const Influx = require('influxdb-nodejs');
 
 
@@ -6,24 +8,84 @@ module.exports.getAquariumById = getAquariumById;
 module.exports.getAquariumByIdDatas = getAquariumByIdDatas ;
 module.exports.getAquariumByIdLight = getAquariumByIdLight;
 module.exports.getAquariumByIdFood = getAquariumByIdFood;
+module.exports.insertPhValuesByAquariumId = insertPhValuesByAquariumId;
+module.exports.getPhValuesByAquariumId = getPhValuesByAquariumId;
 
 
-function getUserById(influx_client, userId) {
-    return influx_client.query('http')
-        .where('location', userId)
-        .then((value)=>{
-            console.log(value);
-            return value;
+function getUserById(users, userId) {
+    return users.find(user => user.userid == userId);
+}
+
+function getAquariumById(users, aquariumId) {
+
+    let result =  null;
+    users.forEach((u)=>{
+        let aquarium = u.aquariums.find(aqu => aqu.id == aquariumId);
+        if (aquarium!==undefined){
+            result = aquarium;
+        }
+    });
+    return result;
+}
+
+function getPhValuesByAquariumId(client,aquariumId,userId, response) {
+
+
+    client.query('ph_sensor')
+        .where('aquarium', `${aquariumId}`)
+        .where('user', `${userId}`)
+        .then((values)=>{
+            console.log(values.results[0].series[0].values);
+            let value =  values.results[0].series[0].values;
+
+            response.json({
+                data:  value,
+                status:true
+            })
+        })
+        .catch(console.error);
+
+
+}
+
+function insertPhValuesByAquariumId(client,aquariumId, userId, value) {
+
+    return client.write('ph_sensor')
+        .tag({
+            aquarium: aquariumId,
+            user: userId
+        })
+        .field({
+            value:value
+        })
+        .then(() => {
+            return true;
         })
         .catch((err)=>{
             console.log(err);
-            return null;
+            return false;
         });
 }
 
-function getAquariumById(influx_client, aquariumId) {
-    return [];
+function getLightValuesByAquariumId(client,id) {
+    return client.query('light_sensor')
+        .where('aquariumId', id)
+        .then((values)=>{
+            return values;
+        })
+        .catch(console.error);
 }
+
+function getWaterLvlValuesByAquariumId(client,id) {
+    return client.query('water_sensor')
+        .where('aquariumId', id)
+        .then((values)=>{
+            return values;
+        })
+        .catch(console.error);
+}
+
+
 
 function getAquariumByIdDatas(influx_client, aquariumId) {
     return [];
